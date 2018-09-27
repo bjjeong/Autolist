83b9fa89a06b53f50f966ca675ba23b552000d0f
bjjeong <bjjeong@umich.edu>
2018-09-26 17:18:05 -0700

    Finish styling homepage

--- frontend/components/home/home.jsx
+++ frontend/components/home/home.jsx
@@ -16,26 +16,34 @@ class Home extends Component {
     this.props.history.push('/results');
   }
 
+  update(field) {
+    return e => {
+      this.setState({ [field]: e.target.value });
+    };
+  }
+
   render() {
 
     return(
-      <div className="home-page row">
-        <h1 className="col s12"> Home Page </h1>
-        <form onSubmit={this.handleSubmit} className="col s12">
-          <div className="row">
-            <div className="input-field col s6">
-              <input placeholder="$0" id="min_price" type="number" class="validate" />
-              <label for="min_price">Please Enter Minimum Price</label>
+      <div className="home-page">
+        <div className="form-container row z-depth-2">
+          <form onSubmit={this.handleSubmit} className="col s12">
+            <h1 className="title"> Autolist Vehicle Search </h1>
+            <div className="row">
+              <div className="input-field col s12">
+                <input placeholder="$0" id="min_price" type="number" class="validate" />
+                <label for="min_price">Please Enter Minimum Price</label>
+              </div>
             </div>
-          </div>
-          <div className="row">
-            <div className="input-field col s6">
-              <input placeholder="$0" id="max_price" type="number" class="validate" />
-              <label for="max_price">Please Enter Maximum Price</label>
+            <div className="row">
+              <div className="input-field col s12">
+                <input placeholder="$0" id="max_price" type="number" class="validate" />
+                <label for="max_price">Please Enter Maximum Price</label>
+              </div>
             </div>
-          </div>
-          <button type="submit" className="btn waves-effect waves-light">Search Vehicles</button>
-        </form>
+            <button type="submit" className="btn waves-effect waves-light">Search Vehicles</button>
+          </form>
+        </div>
       </div>
     );
   }
